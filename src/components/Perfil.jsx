0d16ca37761b7a529ee278527b92f10637d52c5c
salmaguiarr514 <salmaguiarr514@gmail.com>
2026-09-01 18:47:04 -0300

    Simplificar componente Perfil con h1, h2 e img

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,16 +1,10 @@
-import { Card, Avatar, Typography } from "antd";
-
-const { Text } = Typography;
-
 const Perfil = ({ nombre, apellido, foto }) => {
   return (
-    <Card className="perfil-card" size="small">
-      <Card.Meta
-        avatar={<Avatar src={foto} size={64} />}
-        title={`${nombre} ${apellido}`}
-        description={<Text type="secondary">Perfil de prueba</Text>}
-      />
-    </Card>
+    <div className="perfil-card">
+      <img src={foto} alt={`${nombre} ${apellido}`} className="perfil-foto" />
+      <h1>{nombre} {apellido}</h1>
+      <h2>Perfil de prueba</h2>
+    </div>
   );
 };
 
